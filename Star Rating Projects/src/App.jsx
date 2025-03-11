0de09b4465667ffdee42e5178de066b8a5deed1c
alimusahaf114 <musahafali43@gmail.com>
2025-03-12 03:01:47 +0500

    I have made start rating component from book learning react by Alen Bank and has understood the logic and updated the code

---
+++ Star Rating Projects/src/App.jsx
@@ -0,0 +1,10 @@
+import StarRating from "./Component/StarRating";
+function App() {
+  return (
+    <>
+      <StarRating></StarRating>
+    </>
+  );
+}
+
+export default App;
---
+++ Star Rating Projects/src/Component/StarRating.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Star from "./Star";
+export default function StarRating({ totalStars = 10 }) {
+  const createArray = (length) => [...Array(length)];
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+}
